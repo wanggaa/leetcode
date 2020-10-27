dd988d7477ccf33a89aa1cbc5786e891e693f6a4
wanggaa <wanggaa@users.noreply.github.com>
2020-10-27 14:21:46 +0800

    update 1072.flip-columns-for-maximum-number-of-equal-rows.py

---
+++ 1072.flip-columns-for-maximum-number-of-equal-rows.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode.cn id=1072 lang=python3
+#
+# [1072] 链表中的下一个更大节点
+#
+# https://leetcode-cn.com/problems/flip-columns-for-maximum-number-of-equal-rows/description/
+#
+# algorithms
+# Medium (54.74%)
+# Total Accepted:    2.3K
+# Total Submissions: 4.1K
+# Testcase Example:  '[[0,1],[1,1]]'
+#
+# 给定由若干 0 和 1 组成的矩阵 matrix，从中选出任意数量的列并翻转其上的 每个 单元格。翻转后，单元格的值从 0 变成 1，或者从 1 变为 0
+# 。
+# 
+# 返回经过一些翻转后，行上所有值都相等的最大行数。
+# 
+# 
+# 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 输入：[[0,1],[1,1]]
+# 输出：1
+# 解释：不进行翻转，有 1 行所有值都相等。
+# 
+# 
+# 示例 2：
+# 
+# 输入：[[0,1],[1,0]]
+# 输出：2
+# 解释：翻转第一列的值之后，这两行都由相等的值组成。
+# 
+# 
+# 示例 3：
+# 
+# 输入：[[0,0,0],[0,0,1],[1,1,0]]
+# 输出：2
+# 解释：翻转前两列的值之后，后两行由相等的值组成。
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= matrix.length <= 300
+# 1 <= matrix[i].length <= 300
+# 所有 matrix[i].length 都相等
+# matrix[i][j] 为 0 或 1
+# 
+# 
+#
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        d = {}
+        for line in matrix:
+            if line[0] == 1:
+                line = [1-x for x in line]
+            k = tuple(line)
+            d[k] = d.get(k,0) + 1
+        return max(d.values()) 
