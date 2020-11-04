80cec54f751e1b2e0eb1df2479f513ec75e078a0
wanggaa <https://github.com/wanggaa>
2020-11-04 11:13:19 +0800

    168.excel-sheet-column-title.py

---
+++ 168.excel-sheet-column-title.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode.cn id=168 lang=python3
+#
+# [168] Excel表列名称
+#
+# https://leetcode-cn.com/problems/excel-sheet-column-title/description/
+#
+# algorithms
+# Easy (38.57%)
+# Total Accepted:    37.9K
+# Total Submissions: 98.3K
+# Testcase Example:  '1'
+#
+# 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+# 
+# 例如，
+# 
+# ⁠   1 -> A
+# ⁠   2 -> B
+# ⁠   3 -> C
+# ⁠   ...
+# ⁠   26 -> Z
+# ⁠   27 -> AA
+# ⁠   28 -> AB 
+# ⁠   ...
+# 
+# 
+# 示例 1:
+# 
+# 输入: 1
+# 输出: "A"
+# 
+# 
+# 示例 2:
+# 
+# 输入: 28
+# 输出: "AB"
+# 
+# 
+# 示例 3:
+# 
+# 输入: 701
+# 输出: "ZY"
+# 
+# 
+#
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        ans = ''
+        
+        while n != 0:
+            t = (n-1) % 26
+            ans = alphabet[t] + ans
+            n = (n-1) // 26
+        return ans
+
+
