cfaee8638a101468b16eff2c7d924b3fff715b85
wanggaa <https://github.com/wanggaa>
2020-11-12 11:41:10 +0800

    638.shopping-offers.py

---
+++ 638.shopping-offers.py
@@ -0,0 +1,95 @@
+#
+# @lc app=leetcode.cn id=638 lang=python3
+#
+# [638] 大礼包
+#
+# https://leetcode-cn.com/problems/shopping-offers/description/
+#
+# algorithms
+# Medium (58.21%)
+# Total Accepted:    7.6K
+# Total Submissions: 13K
+# Testcase Example:  '[2,5]\n[[3,0,5],[1,2,10]]\n[3,2]'
+#
+# 在LeetCode商店中， 有许多在售的物品。
+# 
+# 然而，也有一些大礼包，每个大礼包以优惠的价格捆绑销售一组物品。
+# 
+# 现给定每个物品的价格，每个大礼包包含物品的清单，以及待购物品清单。请输出确切完成待购清单的最低花费。
+# 
+# 每个大礼包的由一个数组中的一组数据描述，最后一个数字代表大礼包的价格，其他数字分别表示内含的其他种类物品的数量。
+# 
+# 任意大礼包可无限次购买。
+# 
+# 示例 1:
+# 
+# 输入: [2,5], [[3,0,5],[1,2,10]], [3,2]
+# 输出: 14
+# 解释: 
+# 有A和B两种物品，价格分别为¥2和¥5。
+# 大礼包1，你可以以¥5的价格购买3A和0B。
+# 大礼包2， 你可以以¥10的价格购买1A和2B。
+# 你需要购买3个A和2个B， 所以你付了¥10购买了1A和2B（大礼包2），以及¥4购买2A。
+# 
+# 示例 2:
+# 
+# 输入: [2,3,4], [[1,1,0,4],[2,2,1,9]], [1,2,1]
+# 输出: 11
+# 解释: 
+# A，B，C的价格分别为¥2，¥3，¥4.
+# 你可以用¥4购买1A和1B，也可以用¥9购买2A，2B和1C。
+# 你需要买1A，2B和1C，所以你付了¥4买了1A和1B（大礼包1），以及¥3购买1B， ¥4购买1C。
+# 你不可以购买超出待购清单的物品，尽管购买大礼包2更加便宜。
+# 
+# 
+# 说明:
+# 
+# 
+# 最多6种物品， 100种大礼包。
+# 每种物品，你最多只需要购买6个。
+# 你不可以购买超出待购清单的物品，即使更便宜。
+# 
+# 
+#
+class Solution:
+    def shoppingOffers(self, price: List[int], special: List[List[int]], needs: List[int]) -> int: 
+        # 不可超过总数
+        # price 可作为单独礼包处理
+        # 递归+记忆化？
+        # 从少变多的DP？
+        
+        # p_num = len(price)
+        # for i in range(p_num):
+        #     t = [0] * (p_num+1)
+        #     t[i] = 1
+        #     t[-1] = price[i]
+        #     special.append(t)
+
+        d = {}
+        d[tuple([0]*len(needs))] = 0
+
+        def helper(price,special,needs):
+            key = tuple(needs)
+            if key in d:
+                return d[key]
+
+            answers = []
+            answers.append(sum([price[i]*needs[i] for i in range(len(needs))]))
+            for sp in special:
+                tneeds = needs.copy()
+                valid = True
+                for i in range(len(needs)):
+                    if needs[i] >= sp[i]:
+                        tneeds[i] -= sp[i]
+                        continue
+                    valid = False
+                    break
+                if valid:
+                    ans = helper(price,special,tneeds)+sp[-1]
+                    answers.append(ans)
+                    ans = sum([tneeds[i]*price[i] for i in range(len(tneeds))])+sp[-1]
+                    answers.append(ans)
+            d[key] = answer = min(answers)
+            return answer
+
+        return helper(price,special,needs)
