244869ba825f855d018e5b9da9ef3425cd699cbb
wanggaa <https://github.com/wanggaa>
2020-11-23 18:35:22 +0800

    231.power-of-two.py

---
+++ 231.power-of-two.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=231 lang=python3
+#
+# [231] 2的幂
+#
+# https://leetcode-cn.com/problems/power-of-two/description/
+#
+# algorithms
+# Easy (48.68%)
+# Total Accepted:    85.2K
+# Total Submissions: 174.9K
+# Testcase Example:  '1'
+#
+# 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+# 
+# 示例 1:
+# 
+# 输入: 1
+# 输出: true
+# 解释: 2^0 = 1
+# 
+# 示例 2:
+# 
+# 输入: 16
+# 输出: true
+# 解释: 2^4 = 16
+# 
+# 示例 3:
+# 
+# 输入: 218
+# 输出: false
+# 
+#
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n >= 0 and bin(n).count('1') == 1
