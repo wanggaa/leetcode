e02839001b0b1acb86c85e7152a097c1a22e3928
wanggaa <https://github.com/wanggaa>
2020-11-23 19:26:58 +0800

    1128.number-of-equivalent-domino-pairs.py

---
+++ 1128.number-of-equivalent-domino-pairs.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode.cn id=1128 lang=python3
+#
+# [1128] 删除字符串中的所有相邻重复项
+#
+# https://leetcode-cn.com/problems/number-of-equivalent-domino-pairs/description/
+#
+# algorithms
+# Easy (46.40%)
+# Total Accepted:    8.5K
+# Total Submissions: 18.1K
+# Testcase Example:  '[[1,2],[2,1],[3,4],[5,6]]'
+#
+# 给你一个由一些多米诺骨牌组成的列表 dominoes。
+# 
+# 如果其中某一张多米诺骨牌可以通过旋转 0 度或 180 度得到另一张多米诺骨牌，我们就认为这两张牌是等价的。
+# 
+# 形式上，dominoes[i] = [a, b] 和 dominoes[j] = [c, d] 等价的前提是 a==c 且 b==d，或是 a==d 且
+# b==c。
+# 
+# 在 0 <= i < j < dominoes.length 的前提下，找出满足 dominoes[i] 和 dominoes[j] 等价的骨牌对 (i,
+# j) 的数量。
+# 
+# 
+# 
+# 示例：
+# 
+# 输入：dominoes = [[1,2],[2,1],[3,4],[5,6]]
+# 输出：1
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= dominoes.length <= 40000
+# 1 <= dominoes[i][j] <= 9
+# 
+# 
+#
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        d = {} 
+        for dominoe in dominoes:
+            dominoe.sort()
+            t = tuple(dominoe)
+            d[t] = d.get(t,0) + 1
+
+        ans = 0
+        for k in d:
+            if d[k] < 2:
+                continue
+            ans += d[k]*(d[k]-1)//2
+        return ans
