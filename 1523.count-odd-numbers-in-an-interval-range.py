45b8d50811cc3c7c55628df2602aa3203abd74d9
wanggaa <https://github.com/wanggaa>
2020-11-23 18:05:03 +0800

    1523.count-odd-numbers-in-an-interval-range.py

---
+++ 1523.count-odd-numbers-in-an-interval-range.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode.cn id=1523 lang=python3
+#
+# [1523] 股票的资本损益
+#
+# https://leetcode-cn.com/problems/count-odd-numbers-in-an-interval-range/description/
+#
+# algorithms
+# Easy (56.91%)
+# Total Accepted:    5.7K
+# Total Submissions: 10.1K
+# Testcase Example:  '3\n7'
+#
+# 给你两个非负整数 low 和 high 。请你返回 low 和 high 之间（包括二者）奇数的数目。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 输入：low = 3, high = 7
+# 输出：3
+# 解释：3 到 7 之间奇数数字为 [3,5,7] 。
+# 
+# 示例 2：
+# 
+# 输入：low = 8, high = 10
+# 输出：1
+# 解释：8 到 10 之间奇数数字为 [9] 。
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 0 <= low <= high <= 10^9
+# 
+# 
+#
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        t = high - low + 1
+        return t//2 + (t&1 and low&1)
+
