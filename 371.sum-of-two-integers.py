affe2e8f5ce9c5b1fc3b080747b266090844b0e3
wanggaa <https://github.com/wanggaa>
2020-11-23 17:32:50 +0800

    371.sum-of-two-integers.py

---
+++ 371.sum-of-two-integers.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=371 lang=python3
+#
+# [371] 两整数之和
+#
+# https://leetcode-cn.com/problems/sum-of-two-integers/description/
+#
+# algorithms
+# Easy (56.54%)
+# Total Accepted:    40.5K
+# Total Submissions: 71.4K
+# Testcase Example:  '1\n2'
+#
+# # 
+# 示例 1:
+# 
+# 输入: a = 1, b = 2
+# 输出: 3
+# 
+# 
+# 示例 2:
+# 
+# 输入: a = -2, b = 3
+# 输出: 1
+# 
+#
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return a+b
+        
