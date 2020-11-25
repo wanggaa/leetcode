311dcdf06c9882d4a505b62fbddc552e13dc98bc
wanggaa <https://github.com/wanggaa>
2020-11-25 17:39:55 +0800

    572.subtree-of-another-tree.py

---
+++ 572.subtree-of-another-tree.py
@@ -0,0 +1,86 @@
+#
+# @lc app=leetcode.cn id=572 lang=python3
+#
+# [572] 另一个树的子树
+#
+# https://leetcode-cn.com/problems/subtree-of-another-tree/description/
+#
+# algorithms
+# Easy (47.08%)
+# Total Accepted:    53.7K
+# Total Submissions: 114.1K
+# Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+#
+# 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s
+# 也可以看做它自身的一棵子树。
+# 
+# 示例 1:
+# 给定的树 s:
+# 
+# 
+# ⁠    3
+# ⁠   / \
+# ⁠  4   5
+# ⁠ / \
+# ⁠1   2
+# 
+# 
+# 给定的树 t：
+# 
+# 
+# ⁠  4 
+# ⁠ / \
+# ⁠1   2
+# 
+# 
+# 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+# 
+# 示例 2:
+# 给定的树 s：
+# 
+# 
+# ⁠    3
+# ⁠   / \
+# ⁠  4   5
+# ⁠ / \
+# ⁠1   2
+# ⁠   /
+# ⁠  0
+# 
+# 
+# 给定的树 t：
+# 
+# 
+# ⁠  4
+# ⁠ / \
+# ⁠1   2
+# 
+# 
+# 返回 false。
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+
+        copyt = t
+        def equal(s,t):
+            if s is None and t is None:
+                return True
+
+            if s is None or t is None:
+                return False
+            
+            ans = False
+            if s.val == t.val:
+                ans = equal(s.left,t.left) and equal(s.right,t.right)
+            if t != copyt:
+                return ans
+            return ans or equal(s.left,t) or equal(s.right,t)
+
+        return equal(s,t)
